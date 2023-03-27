20643540f974fd4dea4d12d034afef5cef502798
Shubhamshinde25 <shubham2002shinde@gmail.com>
2023-03-28 00:48:46 +0530

    operator

---
+++ Operator/8.java
@@ -0,0 +1,6 @@
+class C2W{
+	public static void main(String[] args){
+		int a=10;
+		System.out.println(~a);
+	}
+}
---
+++ Operator/9.java
@@ -0,0 +1,8 @@
+class C2W{
+	static int var=2;
+	public static void main(String[] args){
+		var=(var=4)*(++var);
+
+		System.out.println(var);
+	}
+}
